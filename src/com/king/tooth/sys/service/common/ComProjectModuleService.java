3e8e3798a4be87ec8df5d45717cee22b86dc3f34
DougLei <953034206@qq.com>
2018-06-09 09:41:24 +0800

    优化代码

---
+++ src/com/king/tooth/sys/service/common/ComProjectModuleService.java
@@ -0,0 +1,39 @@
+package com.king.tooth.sys.service.common;
+
+import com.king.tooth.sys.entity.common.ComProjectModule;
+import com.king.tooth.sys.service.AbstractService;
+
+/**
+ * 项目模块信息资源对象处理器
+ * @author DougLei
+ */
+public class ComProjectModuleService extends AbstractService{
+
+	/**
+	 * 保存项目模块
+	 * @param projectModule
+	 * @return
+	 */
+	public String saveProjectModule(ComProjectModule projectModule) {
+		return null;
+	}
+
+	/**
+	 * 修改项目模块
+	 * @param projectModule
+	 * @return
+	 */
+	public String updateProjectModule(ComProjectModule projectModule) {
+		return null;
+	}
+
+	/**
+	 * 删除项目模块
+	 * @param projectModuleId
+	 * @return
+	 */
+	public String deleteProjectModule(String projectModuleId) {
+		return null;
+	}
+	
+}
