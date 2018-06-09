1a29fd49b07080ca9d6bccdf403abbcde358dfad
DougLei <953034206@qq.com>
2018-06-09 11:57:02 +0800

    优化代码

--- src/com/king/tooth/sys/deploy/DeployEntity.java
+++ src/com/king/tooth/sys/deploy/DeployEntity.java
@@ -1,6 +1,12 @@
 package com.king.tooth.sys.deploy;
 
 import java.io.Serializable;
+import java.util.List;
+
+import com.king.tooth.sys.entity.common.ComDatabase;
+import com.king.tooth.sys.entity.common.ComProject;
+import com.king.tooth.sys.entity.common.ComSqlScript;
+import com.king.tooth.sys.entity.common.ComTabledata;
 
 /**
  * 发布实体
@@ -9,5 +15,47 @@ import java.io.Serializable;
  */
 @SuppressWarnings("serial")
 public class DeployEntity implements Serializable{
-
+	/**
+	 * 要发布的数据库对象
+	 */
+	private ComDatabase database;
+	/**
+	 * 要发布的项目对象
+	 */
+	private ComProject project;
+	/**
+	 * 要发布的表集合对象
+	 */
+	private List<ComTabledata> tables;
+	/**
+	 * 要发布的sql脚本集合对象
+	 */
+	private List<ComSqlScript> sqlScripts;
+	
+	// ------------------------------------------------------------------------------------
+	
+	public ComDatabase getDatabase() {
+		return database;
+	}
+	public void setDatabase(ComDatabase database) {
+		this.database = database;
+	}
+	public ComProject getProject() {
+		return project;
+	}
+	public void setProject(ComProject project) {
+		this.project = project;
+	}
+	public List<ComTabledata> getTables() {
+		return tables;
+	}
+	public void setTables(List<ComTabledata> tables) {
+		this.tables = tables;
+	}
+	public List<ComSqlScript> getSqlScripts() {
+		return sqlScripts;
+	}
+	public void setSqlScripts(List<ComSqlScript> sqlScripts) {
+		this.sqlScripts = sqlScripts;
+	}
 }
