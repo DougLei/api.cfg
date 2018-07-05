054407ad2d01fc8dfcf5090c9460da613cd23eb1
DougLei <953034206@qq.com>
2018-07-05 17:11:35 +0800

    优化代码

--- src/test/Sub.java
+++ src/test/Sub.java
@@ -1,8 +1,16 @@
 package test;
 
+import com.alibaba.fastjson.JSONArray;
+
 public class Sub extends Parent{
 	private String name;
 	public String getName(){
 		return name;
+	}
+	
+	public static void main(String[] args) {
+		String sqlScriptParameterJson = "[]";
+		JSONArray sqlScriptParametersJsonArray = JSONArray.parseArray(sqlScriptParameterJson );
+		System.out.println(sqlScriptParametersJsonArray);
 	}
 }
