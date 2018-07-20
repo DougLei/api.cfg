f41ec923879cb99cf8406f73459c1ac6bc84ca27
DougLei <953034206@qq.com>
2018-07-20 14:39:56 +0800

    优化代码

--- src/com/king/tooth/sys/entity/common/ComPosition.java
+++ src/com/king/tooth/sys/entity/common/ComPosition.java
@@ -1,7 +1,6 @@
 package com.king.tooth.sys.entity.common;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import com.alibaba.fastjson.annotation.JSONField;
@@ -37,12 +36,7 @@ public class ComPosition extends BasicEntity implements ITable, IEntity{
 	 */
 	private Integer orderCode;
 	
-	private Integer booleanVar;
-	private Double doubleVar;
-	private Date dateVar;
-	
 	// ---------------------------------------------------------------------------
-
 	public String getName() {
 		return name;
 	}
@@ -104,25 +98,6 @@ public class ComPosition extends BasicEntity implements ITable, IEntity{
 		orderCodeColumn.setOrderCode(4);
 		columns.add(orderCodeColumn);
 		
-		ComColumndata booleanVarColumn = new ComColumndata("boolean_var", BuiltinCodeDataType.BOOLEAN, 0);
-		booleanVarColumn.setName("布尔值测试");
-		booleanVarColumn.setComments("布尔值测试");
-		booleanVarColumn.setOrderCode(5);
-		columns.add(booleanVarColumn);
-		
-		ComColumndata doubleVarColumn = new ComColumndata("double_var", BuiltinCodeDataType.DOUBLE, 6);
-		doubleVarColumn.setName("浮点型测试");
-		doubleVarColumn.setComments("浮点型测试");
-		doubleVarColumn.setPrecision(2);
-		doubleVarColumn.setOrderCode(6);
-		columns.add(doubleVarColumn);
-		
-		ComColumndata dateVarColumn = new ComColumndata("date_var", BuiltinCodeDataType.DATE, 4);
-		dateVarColumn.setName("日期型测试");
-		dateVarColumn.setComments("日期型测试");
-		dateVarColumn.setOrderCode(7);
-		columns.add(dateVarColumn);
-		
 		table.setColumns(columns);
 		return table;
 	}
@@ -134,24 +109,5 @@ public class ComPosition extends BasicEntity implements ITable, IEntity{
 	@JSONField(serialize = false)
 	public String getEntityName() {
 		return "ComPosition";
-	}
-	
-	public Integer getBooleanVar() {
-		return booleanVar;
-	}
-	public void setBooleanVar(Integer booleanVar) {
-		this.booleanVar = booleanVar;
-	}
-	public Double getDoubleVar() {
-		return doubleVar;
-	}
-	public void setDoubleVar(Double doubleVar) {
-		this.doubleVar = doubleVar;
-	}
-	public Date getDateVar() {
-		return dateVar;
-	}
-	public void setDateVar(Date dateVar) {
-		this.dateVar = dateVar;
 	}
 }
