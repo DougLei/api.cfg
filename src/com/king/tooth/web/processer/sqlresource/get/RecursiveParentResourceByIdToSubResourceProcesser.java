b8aa3102d026cef80a44134b3d4512b331a9fe2e
DougLei <953034206@qq.com>
2018-08-03 18:21:48 +0800

    优化代码

--- src/com/king/tooth/web/processer/sqlresource/get/RecursiveParentResourceByIdToSubResourceProcesser.java
+++ src/com/king/tooth/web/processer/sqlresource/get/RecursiveParentResourceByIdToSubResourceProcesser.java
@@ -1,9 +1,12 @@
 package com.king.tooth.web.processer.sqlresource.get;
 
+import java.util.List;
+import java.util.Map;
+
 import org.hibernate.Query;
 
 import com.king.tooth.sys.entity.common.ComSqlScript;
-import com.king.tooth.web.entity.resulttype.TextResultEntity;
+import com.king.tooth.web.entity.resulttype.PageResultEntity;
 
 /**
  * 处理这种请求路径格式的处理器：/{parentResourceType}/{parentId}/{resourceType}
@@ -18,22 +21,25 @@ public final class RecursiveParentResourceByIdToSubResourceProcesser extends Get
 	
 	protected boolean doGetProcess() {
 		ComSqlScript sqlScriptResource = builtinSqlScriptMethodProcesser.getSqlScriptResource();
-		if(sqlScriptResource.getSqlQueryResultColumnList() != null){
-			validIdColumnIsExists(sqlScriptResource);
-		}
 		
-		String querySql = sqlScriptResource.getFinalSqlScript().getFinalCteSql() + 
-						  getFromSql().toString();
+		String coreQuerySql = sqlScriptResource.getFinalSqlScript().getFinalCteSql()+
+				builtinQueryMethodProcesser.getSql().append(getFromSql());
+		String querySql = coreQuerySql + builtinSortMethodProcesser.getSql();
+		
+		processSelectSqlQueryResultColumns(sqlScriptResource, coreQuerySql);
+		
 		Query query = createQuery(0, querySql);
-		long totalCount = (long) query.uniqueResult();
-		TextResultEntity textResult = new TextResultEntity(totalCount);
-		installResponseBodyForQueryCounter(textResult, true);
+		PageResultEntity pageResultEntity = loadPageResultEntity(query);
+		List<Map<String, Object>> dataList = executeList(query, sqlScriptResource.getSqlQueryResultColumnList());
+		dataList = doProcessDataCollection(dataList);
+		installResponseBodyForQueryDataList(dataList, pageResultEntity, true);
 		return true;
 	}
-	
+
 	protected StringBuilder getFromSql() {
-		StringBuilder sql = new StringBuilder(" select count(1) from (");
-		sql.append(builtinSqlScriptMethodProcesser.getSqlScriptResource().getFinalSqlScript().getFinalSelectSqlScript())
+		StringBuilder sql = new StringBuilder();
+		sql.append(" from ( ")
+		   .append(builtinSqlScriptMethodProcesser.getSqlScriptResource().getFinalSqlScript().getFinalSelectSqlScript())
 		   .append(" ) s_ ")
 		   .append(builtinQueryCondMethodProcesser.getSql());
 		return sql;
