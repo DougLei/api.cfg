f5d5eab0bd1e6d5b2acfe8c758d602969a9c2cc2
DougLei <953034206@qq.com>
2018-11-13 18:37:34 +0800

    tmp

---
+++ src/com/king/tooth/sys/entity/cfg/sql/SqlExecutor.java
@@ -0,0 +1,138 @@
+package com.king.tooth.sys.entity.cfg.sql;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.Query;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.king.tooth.constants.OperDataTypeConstants;
+import com.king.tooth.constants.ResourcePropNameConstants;
+import com.king.tooth.constants.SqlStatementTypeConstants;
+import com.king.tooth.plugins.alibaba.json.extend.string.IJson;
+import com.king.tooth.sys.entity.cfg.CfgPropCodeRule;
+import com.king.tooth.sys.entity.cfg.CfgSql;
+import com.king.tooth.thread.current.CurrentThreadContext;
+import com.king.tooth.util.Log4jUtil;
+import com.king.tooth.util.database.ProcedureUtil;
+import com.king.tooth.util.hibernate.HibernateUtil;
+import com.king.tooth.web.entity.request.valid.data.util.entity.SqlParamSetActualValueEntity;
+
+/**
+ * sql执行者
+ * @author DougLei
+ */
+@SuppressWarnings("serial")
+public class SqlExecutor implements Serializable{
+	
+	private List<List<Object>> sqlParameterValues;
+	
+	/**
+	 * 执行修改的sql语句
+	 * <p>增删改</p>
+	 * @param sql
+	 * @param data
+	 * @param rules
+	 * @return
+	 */
+	public Object doExecuteModifySql(CfgSql sql, List<List<Object>> sqlParameterValues, IJson data, List<CfgPropCodeRule> rules){
+		try {
+			this.sqlParameterValues = sqlParameterValues;
+			
+			List<FinalSqlScriptStatement> finalSqlScriptList = sql.getFinalSqlScriptList();
+			new SqlParamSetActualValueEntity().setFinalCodeVals(sql, rules);
+			
+			String operDataType = null;
+			if(SqlStatementTypeConstants.INSERT.equals(sql.getConfType())){
+				operDataType = OperDataTypeConstants.ADD;
+			}else if(SqlStatementTypeConstants.UPDATE.equals(sql.getConfType())){
+				operDataType = OperDataTypeConstants.EDIT;
+			}else if(SqlStatementTypeConstants.DELETE.equals(sql.getConfType())){
+				operDataType = OperDataTypeConstants.DELETE;
+			}
+			
+			if(SqlStatementTypeConstants.PROCEDURE.equals(sql.getType())){// 是存储过程
+				JSONArray jsonArray = ProcedureUtil.executeProcedureOnDataFocused(sql, operDataType, data);
+				if(jsonArray != null && jsonArray.size() > 0){
+					if(jsonArray.size() == 1){
+						return jsonArray.getJSONObject(0);
+					}else{
+						return jsonArray;
+					}
+				}else{
+					return "成功执行名为["+sql.getObjectName()+"]的存储过程";
+				}
+			}else{
+				String[] modifySqlArr;
+				Query query;
+				int index = 0;
+				for (FinalSqlScriptStatement finalSqlScript : finalSqlScriptList) {
+					modifySqlArr = finalSqlScript.getFinalModifySqlArr();
+					int len = modifySqlArr.length;
+					for (int i = 0; i < len; i++) {
+						query = createQuery(index++, modifySqlArr[i].replace(";", ""));
+						query.executeUpdate();
+					}
+				}
+				
+				if(operDataType != null){
+					int size = data.size();
+					JSONObject jsonObject;
+					for(int i=0;i<size;i++){
+						jsonObject = data.get(i);
+						if(jsonObject.get(ResourcePropNameConstants.ID) != null){
+							jsonObject.put(ResourcePropNameConstants.FOCUSED_OPER, jsonObject.getString(ResourcePropNameConstants.ID) + "_" + operDataType);
+						}
+					}
+				}
+				return data.getJson();
+			}
+		} finally{
+			// 清除sql语句中的参数值集合
+			if(sqlParameterValues.size() > 0){
+				for(List<Object> list : sqlParameterValues){
+					if(list != null){
+						list.clear();
+					}
+				}
+				sqlParameterValues.clear();
+			}
+		}
+	}
+	
+	/**
+	 * 创建查询对象
+	 * @param index
+	 * @param sql
+	 * @param
+	 */
+	protected final Query createQuery(int index, String sql){
+		Query query = HibernateUtil.getCurrentThreadSession().createSQLQuery(sql);
+		setQueryCondParamters(index, query);
+		
+		Log4jUtil.debug("【最后执行的sql语句为：{}】", sql);
+		Log4jUtil.debug("【最后执行的sql语句对应的条件值集合为：{}】", sqlParameterValues.size()>0?sqlParameterValues.get(index):null);
+		
+		// 日志记录发出的hql/sql语句
+		CurrentThreadContext.toReqLogDataAddOperSqlLog(sql, sqlParameterValues.size()>0?sqlParameterValues.get(index):null);
+		return query;
+	}
+	
+	/**
+	 * 给查询对象query设置各个条件的值
+	 * @param index
+	 * @param query
+	 */
+	private void setQueryCondParamters(int index, Query query){
+		if(sqlParameterValues.size() > 0){
+			List<Object> querySqlParameterValues = sqlParameterValues.get(index);
+			if(querySqlParameterValues != null && querySqlParameterValues.size() > 0){
+				int i = 0;
+				for (Object val : querySqlParameterValues) {
+					query.setParameter(i++, val);
+				}
+			}
+		}
+	}
+}
