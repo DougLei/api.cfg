1c270201c4550887292780da4fb42780ad87e46e
DougLei <953034206@qq.com>
2018-11-29 16:02:28 +0800

    t

--- src/com/king/tooth/workflow/entity/node/ProcessNode.java
+++ src/com/king/tooth/workflow/entity/node/ProcessNode.java
@@ -1,5 +1,9 @@
 package com.king.tooth.workflow.entity.node;
 
+import java.util.List;
+
+import com.king.tooth.workflow.entity.node.connection.SequenceFlow;
+
 /**
  * process节点
  * @author DougLei
@@ -11,6 +15,15 @@ public class ProcessNode extends ProcDesginNode{
 	}
 	
 	/**
+	 * 进入的顺序流对象集合
+	 */
+	protected List<SequenceFlow> inSequenceFlows;
+	/**
+	 * 出去的顺序流对象集合
+	 */
+	protected List<SequenceFlow> outSequenceFlows;
+	
+	/**
 	 * 备注
 	 */
 	private String remark;
@@ -20,5 +33,17 @@ public class ProcessNode extends ProcDesginNode{
 	}
 	public void setRemark(String remark) {
 		this.remark = remark;
+	}
+	public List<SequenceFlow> getInSequenceFlows() {
+		return inSequenceFlows;
+	}
+	public void setInSequenceFlows(List<SequenceFlow> inSequenceFlows) {
+		this.inSequenceFlows = inSequenceFlows;
+	}
+	public List<SequenceFlow> getOutSequenceFlows() {
+		return outSequenceFlows;
+	}
+	public void setOutSequenceFlows(List<SequenceFlow> outSequenceFlows) {
+		this.outSequenceFlows = outSequenceFlows;
 	}
 }
