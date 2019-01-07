a5ef9ad84daa45969e74fa6420904406c1122b49
DougLei <953034206@qq.com>
2019-01-07 12:41:03 +0800

    t

--- src/com/king/tooth/sys/service/tools/InitSystemService.java
+++ src/com/king/tooth/sys/service/tools/InitSystemService.java
@@ -269,9 +269,7 @@ public class InitSystemService extends AService{
 			// 先加载当前系统数据库的所有hbm映射文件
 			loadCurrentSysDatabaseHbms();
 			// 清空用户在线数据表
-			if(SysContext.getSystemConfig("db.default.ip").equals("localhost") && SysContext.getSystemConfig("is.develop").equals("true")){
-				HibernateUtil.executeUpdateBySql(SqlStatementTypeConstants.DELETE, "truncate table sys_account_online_status", null);
-			}
+			HibernateUtil.executeUpdateBySql(SqlStatementTypeConstants.DELETE, "truncate table sys_account_online_status", null);
 		} catch (Exception e) {
 			Log4jUtil.error("系统初始化出现异常，异常信息为:{}", ExceptionUtil.getErrMsg(e));
 			System.exit(0);
